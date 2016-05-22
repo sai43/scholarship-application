5233ccf4f2fa3efe10a2d8b4fdf42835c0ece2a9
Zane <i.zane.am.robot@gmail.com>
2016-05-22 15:17:14 -0400

    Added params to applications controller which I had forgot to do for fields added via migration

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -69,6 +69,6 @@ class ApplicationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def application_params
-      params.require(:application).permit(:name, :gender, :date_of_birth, :gpa, :essay)
+      params.require(:application).permit(:name, :gender, :date_of_birth, :gpa, :address, :state, :university, :essay)
     end
 end
