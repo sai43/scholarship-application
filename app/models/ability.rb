bb6d8926f9f87c2f9050a37cc4b1c9a38eea3ba5
Zane <i.zane.am.robot@gmail.com>
2016-05-23 20:13:19 -0400

    Updated application model to validate all scholarship application fields

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,8 +6,8 @@ class Ability
     	if user.admin?
     		can :manage, :all
     	else
-    		can :read, User, :id => user.id
-    		can :manage, User, :id => user.id
+    		can :read, Application
+    		can :create, Application
     	end
     end
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,13 @@
 class Application < ActiveRecord::Base
 	belongs_to :user
+
+	#User must fully fill out application
+
+	validates :name, presence: true
+	validates :gender, presence: true
+	validates :date_of_birth, presence: true
+	validates :gpa, presence: true
+	validates :university, presence: true
+	validates :address, presence: true
+	validates :state, presence: true
 end
